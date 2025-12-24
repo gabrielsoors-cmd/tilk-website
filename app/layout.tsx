cae4f222727ee0716c1c45d31e29101213b31996
Gabriel Soors <gabriel.soors@gmail.com>
2025-12-24 22:59:34 +0100

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -28,6 +29,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="sv">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-E9TS1S6Q1E"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E9TS1S6Q1E');
+          `}
+        </Script>
+      </head>
       <body className="bg-bg antialiased">{children}</body>
     </html>
   )
