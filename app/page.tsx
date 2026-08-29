6cb43ce7cc6814a2255916d8fe76ce04cc2cbadf
gabrielsoors-cmd <gabriel.soors@gmail.com>
2026-08-29 17:07:45 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
+import UseCases from '@/components/UseCases'
 import HowItWorks from '@/components/HowItWorks'
+import Trust from '@/components/Trust'
 import PricingTeaser from '@/components/PricingTeaser'
 import Footer from '@/components/Footer'
 
@@ -12,7 +14,9 @@ export default function HomePage() {
       <main>
         <Hero lang="sv" />
         <Features lang="sv" />
+        <UseCases lang="sv" />
         <HowItWorks lang="sv" />
+        <Trust lang="sv" />
         <PricingTeaser lang="sv" />
       </main>
       <Footer lang="sv" />
